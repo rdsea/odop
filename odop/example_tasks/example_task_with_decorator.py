297b67f6431bbc8537eb7fd3c244b9a5884b8baf
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-02-23 13:12:27 +0200

    Make example task faster for testing

--- odop/example_tasks/example_task_with_decorator.py
+++ odop/example_tasks/example_task_with_decorator.py
@@ -4,11 +4,10 @@
 # to get around the syntax error. Although I think we can expand it to
 # implement the task reading functionality.
 
-import time
 from odop import task_manager
 
 @task_manager.odop_task(name="example_task", time="2h", cpu="2", memory="2G")
 def example_task_function():
     # The task can be an arbitrary Python function
-    time.sleep(5)
+    print("Running example task")
 
