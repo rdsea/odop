dad6652e337a8006ebd61f99b0597d9b362553bb
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-03-01 18:52:14 +0200

    Comments about monitoring processes in engine

--- odop/task_runner.py
+++ odop/task_runner.py
@@ -6,6 +6,8 @@ def engine_run_task_from_serialized(task_name):
     to run a task on a node.
 
     Example 1: Running the function from a serialized file
+
+    This should be ran and monitored by the engine.
     """
     with open(f"{task_name}.pickle", "rb") as file:
         task = cloudpickle.load(file)
@@ -17,6 +19,8 @@ def engine_run_task_from_script(task_name):
     to run a task on a node.
 
     Example 2: Running the function from a script
+
+    Capture the process for monitoring and killing if time runs out.
     """
     import subprocess
     process = subprocess.run(["python", f"{task_name}_runner.py"])
