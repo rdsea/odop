e262912d2619270e3e63b31061dea3e474e0428d
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2024-04-05 13:44:59 +0300

    Add tests for the decorators

--- tests/test_task_reader.py
+++ tests/test_task_reader.py
@@ -1,16 +1,15 @@
 """ Tests for the task_manager module. """
 
 import os
-import time
-import pytest
 import odop
+import pandas as pd
 
 module_dir = os.path.dirname(os.path.abspath(odop.__file__))
 examples_dir = os.path.join(module_dir, 'example_tasks')
 
 
 def test_task():
-    @odop.task.task(name="test_name")
+    @odop.task.task("test_name")
     def test_task():
         return "success"
 
@@ -20,4 +19,41 @@ def test_task():
     assert test_task.func() == "success"
     assert test_task.time_limit is None
 
+
+def test_time_limit_and_memory():
+    @odop.task.task("test_name")
+    @odop.task.time_limit("5min")
+    @odop.task.memory_limit("5G")
+    def test_task():
+        return "success"
+
+    assert type(test_task) == odop.task.Task
+
+    assert test_task.name == "test_name"
+    assert test_task.func() == "success"
+    assert test_task.time_limit == pd.to_timedelta("5min")
+    assert test_task.memory_limit == "5G"
+
+
+def test_task_list():
+    @odop.task.task("task 1")
+    def task_1():
+        print("task 1")
+
+    @odop.task.task("task 2")
+    def task_2():
+        print("task 2")
+
+    @odop.task.task("task 3")
+    def task_3():
+        print("task 3")
+
+
+    assert task_1.name == "task 1"
+    assert task_2.name == "task 2"
+    assert task_3.name == "task 3"
+
+    assert odop.task.tasks[-3].name == "task 1"
+    assert odop.task.tasks[-2].name == "task 2"
+    assert odop.task.tasks[-1].name == "task 3"
 
