c7c7790c839c8c9fa2b2a59ef92328181bfa70d8
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2024-04-05 21:56:51 +0300

    Fix module reference.

--- docs/examples/task_specification_decorator.py
+++ docs/examples/task_specification_decorator.py
@@ -1,10 +1,10 @@
 import odop
 
 #example 1
-@odop.task("task 0")
-@odop.time_limit("10min")
-@odop.single_file
-@odop.input_folder("data")
+@odop.task.task("task 0")
+@odop.task.time_limit("10min")
+@odop.task.single_file
+@odop.task.input_folder("data")
 def step_a():
     print("Running task 0")
 #end 1
@@ -24,7 +24,7 @@ odop.tasks.register_task(task)
 
 
 #example 3
-@odop.for_files_in_folder("output_data")
+@odop.task.for_files_in_folder("output_data")
 def step_a(filename):
     print(f"Found file called {filename} in output_data")
 #end 3
