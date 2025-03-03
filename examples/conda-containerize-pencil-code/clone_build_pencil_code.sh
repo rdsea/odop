c63b2edaebc565ab8654fff6e5a02f883a68427d
nguu0123 <nguu0123@gmail.com>
2025-03-03 10:04:38 +0200

    update build

--- examples/conda-containerize-pencil-code/clone_build_pencil_code.sh
+++ examples/conda-containerize-pencil-code/clone_build_pencil_code.sh
@@ -4,26 +4,6 @@ module load CrayEnv
 module load rocm
 module load cray-python
 
-config_file=lumi_gnu.config
-
-tee $config_file <<'EOF' >/dev/null
-%include compilers/GNU-GCC_MPI
-%section Makefile
-        # turn on software optimizations
-        FC = ftn
-        F90 = $(FC)
-        CC = cc
-        FFLAGS += -O3 -g
-        FFLAGS += -mcmodel=large 
-        CFLAGS += -O2 -D_GNU_SOURCE
-        LDFLAGS += -ldl -lpthread 
-%endsection Makefile
-
-%section runtime
-        mpiexec = srun
-%endsection runtime
-EOF
-
 set -ex
 git clone https://github.com/pencil-code/pencil-code.git
 
