b438c156bf70f0998ec04c762f500fc9599aa101
nguu0123 <nguu0123@gmail.com>
2025-03-12 15:17:57 +0200

    add data movement splitted task and config for 2 tasks

--- benchmark/scheduling_algorithms/tasks/upload_data_splitted.py
+++ benchmark/scheduling_algorithms/tasks/upload_data_splitted.py
@@ -10,40 +10,39 @@ logging.getLogger("requests").setLevel(logging.CRITICAL)
 logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
 
 
-@odop.task(
-    name="upload_data_to_allas",
-    trigger="file_updated",
-    file_path="/users/anhdungn/pencil-code2/samples/gputest/data/proc0/var.dat",
-)
-def upload_folder(filenames=None):
-    options = {
-        "auth_version": os.getenv("OS_IDENTITY_API_VERSION"),
-        "user": os.getenv("OS_USERNAME"),
-        "key": os.getenv("OS_PASSWORD"),
-        "authurl": os.getenv("OS_AUTH_URL"),
-        "os_options": {
-            "user_domain_name": os.getenv("OS_USER_DOMAIN_NAME"),
-            "project_domain_name": os.getenv("OS_PROJECT_DOMAIN_NAME"),
-            "project_name": os.getenv("OS_PROJECT_NAME"),
-        },
-    }
+def get_upload_folder(folder_name):
+    @odop.task(
+        name="upload_data_to_allas",
+        trigger=odop.FileUpdated(
+            f"/users/anhdungn/pencil-code2/samples/gputest/data/{folder_name}/var.dat"
+        ),
+        cpus=2,
+    )
+    def upload_folder(filenames=None):
+        options = {
+            "auth_version": os.getenv("OS_IDENTITY_API_VERSION"),
+            "user": os.getenv("OS_USERNAME"),
+            "key": os.getenv("OS_PASSWORD"),
+            "authurl": os.getenv("OS_AUTH_URL"),
+            "os_options": {
+                "user_domain_name": os.getenv("OS_USER_DOMAIN_NAME"),
+                "project_domain_name": os.getenv("OS_PROJECT_DOMAIN_NAME"),
+                "project_name": os.getenv("OS_PROJECT_NAME"),
+            },
+        }
 
-    bucket_name = "2009846-folder"
-    timestamp = int(time.time())
-    with SwiftService(options=options) as swift:
-        try:
-            swift.post(container=bucket_name)
-            print(f"Container '{bucket_name}' created or exists.")
-        except SwiftError as e:
-            print(f"Error creating container '{bucket_name}': {e.value}")
-            return
+        bucket_name = "2009846-folder"
+        timestamp = int(time.time())
+        with SwiftService(options=options) as swift:
+            try:
+                swift.post(container=bucket_name)
+                print(f"Container '{bucket_name}' created or exists.")
+            except SwiftError as e:
+                print(f"Error creating container '{bucket_name}': {e.value}")
+                return
 
-        upload_objects = []
-        folders = [
-            f"/users/anhdungn/pencil-code2/samples/gputest/data/proc{i}"
-            for i in range(32)
-        ]
-        for folder in folders:
+            upload_objects = []
+            folder = f"/users/anhdungn/pencil-code2/samples/gputest/data/{folder_name}/var.dat"
             for root, _, files in os.walk(folder):
                 for file in files:
                     file_path = os.path.join(root, file)
@@ -54,12 +53,18 @@ def upload_folder(filenames=None):
                         )
                     )
 
-        try:
-            for result in swift.upload(bucket_name, upload_objects):
-                if result["success"]:
-                    pass
-                else:
-                    print("Failed to upload file")
-        except SwiftError as e:
-            print(f"Error during upload: {e.value}")
-    print("---------------------- DONE --------------------")
+            try:
+                for result in swift.upload(bucket_name, upload_objects):
+                    if result["success"]:
+                        pass
+                    else:
+                        print("Failed to upload file")
+            except SwiftError as e:
+                print(f"Error during upload: {e.value}")
+        print("---------------------- DONE --------------------")
+
+    return upload_folder
+
+
+for proc_number in range(16):
+    get_upload_folder(f"proc{proc_number}")
