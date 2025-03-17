59afdd6cbb6a4c0624bd966f3d19b7a4e72c3b65
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2025-03-17 20:03:18 +0200

    Add a test for starting odop runtime

---
+++ tests/test_start.py
@@ -0,0 +1,19 @@
+import time
+
+import odop
+
+N = 500
+
+
+def test_start(runtime=5):
+    odop.start(
+        run_name="test",
+        config_file="tests/odop_conf.yaml",
+        task_folder="examples/example_tasks",
+    )
+    time.sleep(runtime)
+    odop.stop()
+
+
+if __name__ == "__main__":
+    test_start(N)
